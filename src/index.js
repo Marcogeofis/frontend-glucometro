6ff692f01a2159713e8e36aabb26f5cbda0ad4cb
Marco Rivera <marcogeofis@gmail.com>
2022-05-09 22:59:38 -0500

    Estoy probando como hacer la evaluacion para cargar los datos con el boton ingresar datos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-let nom = document.getSelection('#nom');
+let nom = document.getElementById('nom');
+let edad = document.getElementById('edad');
+let fecha = document.getElementById('fecha');
+let glu = document.getElementById('azucar');
+let preA = document.getElementById('pa');
+let preB = document.getElementById('pb');
+let lat = document.getElementById('lat');
 
-const boton = document.getSelection('.ingresar_datos').onclick = getData(nom)
 
-function getData(nombre){
-    console.log(nombre);
+
+function getData(){
+    const user = {
+        nombre: nom.value,
+        edad: edad.value,
+        fecha: fecha.value,
+        recordData:[
+            {glucosa:[parseInt(glu.value)]},
+            {presionA: [parseInt(preA.value)]},
+            {presionB:[parseInt(preB.value)]},
+            {latidos:[parseInt(lat.value)]},
+        ]
+    };
+
+    console.log(user);
+
 }
